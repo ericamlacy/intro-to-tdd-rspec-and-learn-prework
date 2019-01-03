ae278adc86a5202a206eb037dd43b1ac62ad3005
Erica Lacy <ericalacy7@gmail.com>
2019-01-03 01:21:53 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,4 @@
     def current_age_for_birth_year(birth_year)
-      puts 2018 - birth_year
+      time.now.year - birth_year
     end
-    def age_of_person == current_age_for_birth_year(1984)
+    
